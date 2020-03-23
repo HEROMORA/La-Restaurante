12eaa9dafc36313946b065adea2fc655630550c0
Omar Alaa <omar.skillful@gmail.com>
2020-03-24 01:33:32 +0200

    Created reading functions for the DishRepository

---
+++ src/restaurant/services/DishRepository.java
@@ -0,0 +1,54 @@
+package restaurant.services;
+
+
+import restaurant.dish.Dish;
+import restaurant.dish.DishType;
+
+import java.util.ArrayList;
+
+public class DishRepository {
+
+    private RestaurantService restaurantService = new RestaurantService();
+    private ArrayList<Dish> dishes;
+
+    public DishRepository()
+    {
+        populateList();
+    }
+
+    private void populateList()
+    {
+        dishes = restaurantService.readDishes();
+    }
+
+    // READING FUNCTIONS
+
+    public ArrayList<Dish> getDishes()
+    {
+        return dishes;
+    }
+
+    public Dish getDishByName(String dishName)
+    {
+        for (Dish dish:dishes)
+        {
+            if (dish.getName().equals(dishName))
+                return dish;
+        }
+
+        return null;
+    }
+
+    public ArrayList<Dish> getDishesByType(DishType dishType)
+    {
+        ArrayList<Dish> _dishes = new ArrayList<>();
+
+        for(Dish dish:dishes)
+        {
+            if (dish.getDishType() == dishType)
+                _dishes.add(dish);
+        }
+
+        return _dishes;
+    }
+}

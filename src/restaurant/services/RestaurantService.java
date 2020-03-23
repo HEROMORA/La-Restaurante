e734365e55820f004215657b86c3c592992d9dc6
Omar Alaa <omar.skillful@gmail.com>
2020-03-24 00:13:04 +0200

    Slight changes and increasing code readability for RestaurantService

--- src/restaurant/services/RestaurantService.java
+++ src/restaurant/services/RestaurantService.java
@@ -1,21 +1,23 @@
 package restaurant.services;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import restaurant.dish.*;
 import restaurant.reservation.Table;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class RestaurantService {
-    private Document connect(){
-        try{
+
+    private Document connect() {
+
+        try {
             File file = new File("data.xml");
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
@@ -28,51 +30,74 @@ public class RestaurantService {
             return null;
         }
     }
-    public ArrayList readTables(){
-        ArrayList tableList = new ArrayList();
-        Document doc = this.connect();
+
+    public ArrayList<Table> readTables() {
+
+        ArrayList<Table> tableList = new ArrayList<Table>();
+
+        Document doc = connect();
+        assert doc != null;
         doc.getDocumentElement().normalize();
         NodeList nodeList = doc.getElementsByTagName("table");
-        for(int i = 0; i < nodeList.getLength();i++){
+
+        for(int i = 0; i < nodeList.getLength(); i++) {
+
             Node node = nodeList.item(i);
-            if(node.getNodeType() == Node.ELEMENT_NODE){
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+
                 Element e = (Element)node;
                 Table table = new Table();
+
                 table.setTableNumber(Integer.parseInt(e.getElementsByTagName("number").item(0).getTextContent()));
                 table.setNumberOfSeats(Integer.parseInt(e.getElementsByTagName("number_of_seats").item(0).getTextContent()));
                 table.setSmoking(Boolean.parseBoolean(e.getElementsByTagName("smoking").item(0).getTextContent()));
+
                 tableList.add(table);
             }
         }
+
         return tableList;
     }
-    public ArrayList<Dish> readDishes(){
+
+
+    public ArrayList<Dish> readDishes() {
+
         ArrayList<Dish> dishesList = new ArrayList<>();
-        Document doc = this.connect();
+
+        Document doc = connect();
         assert doc != null;
         doc.getDocumentElement().normalize();
         NodeList nodeList = doc.getElementsByTagName("dish");
-        for(int i = 0; i < nodeList.getLength();i++){
+
+        for(int i = 0; i < nodeList.getLength(); i++) {
+
             Node node = nodeList.item(i);
-            if(node.getNodeType() == Node.ELEMENT_NODE){
+
+            if(node.getNodeType() == Node.ELEMENT_NODE) {
                 Element e = (Element) node;
-                switch (e.getElementsByTagName("type").item(0).getTextContent()){
+
+                switch (e.getElementsByTagName("type").item(0).getTextContent()) {
                     case "main_course":
                         MainDish mainDish = new MainDish(e.getElementsByTagName("name").item(0).getTextContent(), DishType.MAINDISH, BigDecimal.valueOf(Double.parseDouble(e.getElementsByTagName("price").item(0).getTextContent())));
                         dishesList.add(mainDish);
                         break;
+
                     case "appetizer":
                         Appetizer appetizer = new Appetizer(e.getElementsByTagName("name").item(0).getTextContent(), DishType.APPETIZER, BigDecimal.valueOf(Double.parseDouble(e.getElementsByTagName("price").item(0).getTextContent())));
                         dishesList.add(appetizer);
                         break;
+
                     case "desert":
                         Desert desert = new Desert(e.getElementsByTagName("name").item(0).getTextContent(), DishType.DESERT, BigDecimal.valueOf(Double.parseDouble(e.getElementsByTagName("price").item(0).getTextContent())));
                         dishesList.add(desert);
                         break;
-                    default:
+
+                    default: throw new Error("Invalid dish type");
                 }
             }
         }
+
         return dishesList;
     }
 }

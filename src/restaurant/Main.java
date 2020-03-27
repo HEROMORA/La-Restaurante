20c4ff673f27a52bced8a9c17a5d6ee860239081
yosraemad <yosraemad14@gmail.com>
2020-03-28 01:44:01 +0200

    Main Page Styling

--- src/restaurant/Main.java
+++ src/restaurant/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui/sample.fxml"));
         primaryStage.setTitle("Restaurant");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
     }
 

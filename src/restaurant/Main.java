1feda72b67916ac54396c77986ecea95c8e3e76f
Omar Alaa <omar.skillful@gmail.com>
2020-03-28 23:03:12 +0200

    Renaming and refactoring gui package and using naming conventions

--- src/restaurant/Main.java
+++ src/restaurant/Main.java
@@ -5,20 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import restaurant.reservation.Reservation;
-import restaurant.services.RestaurantService;
-import restaurant.users.Manager;
-import restaurant.users.User;
-import restaurant.users.UserRole;
 
 import javax.xml.transform.TransformerException;
-import java.util.Calendar;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("gui/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/MainPage.fxml"));
         primaryStage.setTitle("Restaurant");
         primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
